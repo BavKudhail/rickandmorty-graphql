8c6810dd6020c613713f474dee51323d04ba24ae
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-23 20:34:41 +0100

    connected react application to graphQl server

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// importing apollo client, inmemorycache
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// creating a new apollo client (not to be confused with ApolloServer)
+const client = new ApolloClient({
+  // in this case we are using an external API
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* wrap entire app with an apolloProvider component */}
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
