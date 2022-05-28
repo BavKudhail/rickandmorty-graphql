8a68223b3260f91b90725c5e84b17093b91fb1e3
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-05-28 13:14:08 +0100

    commenting file

--- client/src/pages/Character/Character.js
+++ client/src/pages/Character/Character.js
@@ -1,14 +1,18 @@
 import React from "react";
+// importing hook
 import { useCharacter } from "../../hooks/useCharacter";
 
 const Character = () => {
-  const { loading, data, error } = useCharacter(2);
+  // destructure loading, data and error from useCharacter
+  const { loading, data, error } = useCharacter(5);
   if (loading) return <div>spinner...</div>;
   if (error) return <div>Oops! my bad!</div>;
+
   const character = data.character;
   console.log(character);
   return (
     <>
+    {/* pass in character data within the specific fields */}
       <div className="character">
         <p>{character.name}</p>
         <p>{character.gender}</p>
